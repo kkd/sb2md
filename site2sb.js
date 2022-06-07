6a03a227cd7604961eb566eb13145d6936fc309e
Takeshi Kakeda <takeshi@giantech.jp>
2022-06-07 22:33:01 +0900

    replace slash to dot.

--- site2sb.js
+++ site2sb.js
@@ -1,23 +1,18 @@
-const path = './test/test.json';
+const path = './life-kaizen.json';
 const site = require(path);
 const fs = require('fs');
 
 const site2sb = () => {
   console.log(site.pages);
   fs.mkdirSync('pages');
-  /*
-  for (let i=0; i < site.pages.length; i++) {
-    console.log(site.pages[i].title);
-  }
-  */
+
   for (let i in site.pages) {
     const page = site.pages[i]
     const content = JSON.stringify(page);
-    fs.writeFileSync('pages/' + page.title + ".json", content);
+    const fileName = page.title.replaceAll('/','.') + '.json'
+    fs.writeFileSync('pages/' + fileName, content);
     console.log("wrote " + page.title + ".json");
   }
 }
 
 site2sb();
-
-// exports.site2sb = site2sb;
