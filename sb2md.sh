082a52b8770692f0c59dba14f5ed91ccbe618520
Takeshi Kakeda <takeshi@giantech.jp>
2022-06-08 08:25:04 +0900

    Markdown変換いちおうできた

--- sb2md.sh
+++ sb2md.sh
@@ -5,11 +5,12 @@ rm -rf pages && \
   rm -rf mds && \
   mkdir mds
 
-node site2sb.js && \
-for f in `\find pages -maxdepth 1 -type f`; do
-  # fname=`echo $f | sed -e 's/\ /\\\\ /g'`
-  bname=`basename $f .json`
-  # echo "mds/$bname.md"
-  node cli.js $f > "mds/$bname.md"
-done;
+node site2sb.js
+# && \
+# for f in `\find pages -maxdepth 1 -type f`; do
+#   # fname=`echo $f | sed -e 's/\ /\\\\ /g'`
+#   bname=`basename $f .json`
+#   # echo "mds/$bname.md"
+#   node cli.js $f > "mds/$bname.md"
+# done;
 
--- site2sb.js
+++ site2sb.js
@@ -1,6 +1,7 @@
 const path = './life-kaizen.json';
 const site = require(path);
 const fs = require('fs');
+const { sb2md } = require('./lib/sb2md');
 
 const site2sb = () => {
   console.log(site.pages);
@@ -8,10 +9,17 @@ const site2sb = () => {
 
   for (let i in site.pages) {
     const page = site.pages[i]
-    const content = JSON.stringify(page);
-    const fileName = page.title.replaceAll('/','.') + '.json'
+
+    //一行目は削除する（タイトルなので）
+    const lines = page.lines.splice(0,1);
+
+    // MDに変換
+    const content = sb2md(page.lines.join('\n'))
+
+    //ファイル名設定（スラッシュは.に変換）
+    const fileName = page.title.replaceAll('/','.') + '.md'
     fs.writeFileSync('pages/' + fileName, content);
-    console.log("wrote " + page.title + ".json");
+    console.log("wrote " + page.title + ".md");
   }
 }
 
